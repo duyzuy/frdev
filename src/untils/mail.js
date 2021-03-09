98ece0bff69743476bcf522464588f434db7d03d
duyvutruong <vutruongduy2109@gmail.com>
2021-03-09 14:35:35 +0700

    update mail

--- src/untils/mail.js
+++ src/untils/mail.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function sendMail(){
     return (
-        <div>template</div>
+        <div>templat2222e</div>
     )
 }
 
--- src/untils/test.js
+++ src/untils/test.js
@@ -1,7 +1,9 @@
 import React from 'react';
 
 function Test(){
-
+    return (
+        <div>test</div>
+    )
 }
 
 export default Test;
